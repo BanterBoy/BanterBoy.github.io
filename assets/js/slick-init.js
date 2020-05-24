90f8a84c2c81ba8c43a332fc8152e3f9f7f0ad2b
Luke Leigh <luke.leigh@gmail.com>
2020-05-24 23:46:36 +0100

    Update slick-init.js

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -10,8 +10,51 @@ $(document).ready(function () {
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        slidesToShow: 3,
-        slidesToScroll: 3
         adaptiveHeight: true
     });
+
+    $('.slick-slideshow').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        adaptiveHeight: true
+    });
+
+    $('.slick-variable-width').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: true,
+        variableWidth: true
+    });
+
+    $('.slick-center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
 });
