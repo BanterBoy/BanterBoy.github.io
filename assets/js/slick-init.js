b23c45dd6f50f0e1b9fecb962c9f53bf2461e6ad
Luke Leigh <luke.leigh@gmail.com>
2020-05-31 01:00:45 +0100

    Update slick-init.js

--- assets/js/slick-init.js
+++ assets/js/slick-init.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     });
 
     $('.slick-autoplay').slick({
-        slidesToShow: 7,
+        slidesToShow: 8,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1200
