a8c452fbf0b7a6f1590f5ccb18574f4eb0d14284
Luke Leigh <luke.leigh@gmail.com>
2020-08-16 23:09:19 +0100

    fancy box and remove minimal mistakes image popup class

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -82,9 +82,9 @@ $(document).ready(function() {
   }
 
   // add lightbox class to all image links
-  $(
-    "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
-  ).addClass("image-popup");
+  // $(
+  //   "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
+  // ).addClass("image-popup");
 
   // Magnific-Popup options
   $(".image-popup").magnificPopup({
